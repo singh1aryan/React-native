507457d76b07aea4793d06f7e8f3fa561957e027
aryan <aryansingh@umass.edu>
2019-03-29 15:30:58 -0400

    custom row updated

--- components/CustomRow.js
+++ components/CustomRow.js
@@ -1,5 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, {Component} from 'react';
+import { View, Alert, Text, TouchableHighlight, StyleSheet, Image } from 'react-native';
+import { Button } from 'react-native-elements';
+import AccessibleButton from '../components/AccessibleButton'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+class CustomRow extends Component{
+    constructor(props) {
+        super(props);
+    }
+    
+    render(){
+        return(
+            <View style={styles.container}>
+                {/* <Image source={{ uri: image_url }} style={styles.photo} /> */}
+                {/* Star image to show that it is a favorite */}
+                {/* <AccessibleButton style = {styles.button}/> */}
+                <Image source={{uri: 'http://clipart-library.com/images/6cy5GXj9i.jpg'}}
+                    style = {styles.starImage}
+                />
+                <TouchableOpacity style={styles.container_text} onPress={this.props.navigateAction}>
+                    <Text style={styles.title} >
+                        {this.props.title}
+                    </Text>
+                    <Text style={styles.description}>
+                        {this.props.description}
+                    </Text>
+                </TouchableOpacity>
+                {/* button to delete, accessible button */}
+                <TouchableHighlight onPress={() => this.props.deleteAction(this.props.itemKey)}>
+                    <Image 
+                        source={{uri:'https://previews.123rf.com/images/arhimicrostok/arhimicrostok1706/arhimicrostok170600362/79549735-trash-can-icon-delete-move-to-trash-clear-the-disk-space-vector-illustration-.jpg'}}
+                        style = {styles.deleteButton}
+                    />
+                </TouchableHighlight>
+
+            </View>     
+            );
+        }
+    }
+
+export default CustomRow;
 
 const styles = StyleSheet.create({
     container: {
@@ -19,7 +60,7 @@ const styles = StyleSheet.create({
         color: '#000',
     },
     container_text: {
-        flex: 1,
+        flex: 5,
         flexDirection: 'column',
         marginLeft: 12,
         justifyContent: 'center',
@@ -28,25 +69,20 @@ const styles = StyleSheet.create({
         fontSize: 11,
         fontStyle: 'italic',
     },
-    photo: {
-        height: 50,
-        width: 50,
+    deleteButton: {
+        height: 30,
+        width: 30,
+        justifyContent: 'center'
+    },
+    starImage: {
+        width: 30, 
+        height: 30,
+        marginTop: 5
     },
 });
 
-const CustomRow = ({ title, description, image_url }) => (
-    <View style={styles.container}>
-        <Image source={{ uri: image_url }} style={styles.photo} />
-        <View style={styles.container_text}>
-            <Text style={styles.title}>
-                {title}
-            </Text>
-            <Text style={styles.description}>
-                {description}
-            </Text>
-        </View>
 
-    </View>
-);
-
-export default CustomRow;
+/* one way of adding the image to accessible button is
+   to make a new component AccessibleButtonD - for delete
+   and then add the <Image> inside the touchable Highlight/opacity
+*/
