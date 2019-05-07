225583b0b783f137583d6269101fc8a3346d10d7
aryan <aryansingh@umass.edu>
2019-05-07 19:52:06 -0400

    redux -> reduce, reducer, actions

---
+++ redux/counter.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = { value: 0 };
+
+  increment = () => {
+    this.setState(prevState => ({
+      value: prevState.value + 1
+    }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value - 1
+    }));
+  };
+  
+  render() {
+    return (
+      <div>
+        {this.state.value}
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
+}
+
+// using a reducer, the dispatch method
+// import React, { Component } from 'react';
+
+// const counter = (state = { value: 0 }, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { value: state.value + 1 };
+//     case 'DECREMENT':
+//       return { value: state.value - 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// class Counter extends Component {
+//   state = counter(undefined, {});
+  
+//   dispatch(action) {
+//     this.setState(prevState => counter(prevState, action));
+//   }
+
+//   increment = () => {
+//     this.dispatch({ type: 'INCREMENT' });
+//   };
+
+//   decrement = () => {
+//     this.dispatch({ type: 'DECREMENT' });
+//   };
+  
+//   render() {
+//     return (
+//       <div>
+//         {this.state.value}
+//         <button onClick={this.increment}>+</button>
+//         <button onClick={this.decrement}>-</button>
+//       </div>
+//     )
+//   }
+// }
